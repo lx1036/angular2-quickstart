43a63a7cb9bcf8a3384bea10a5f8c0bee5c6d056
Xiang Liu <xiang.liu@rightcapital.com>
2016-09-15 20:25:43 +0800

    Add *ngFor

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,6 +8,12 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     template: `
         <h1>{{title}}</h1>
+        <h2>My Heroes</h2>
+        <ul class="heroes">
+            <li *ngFor = "let hero of heroes">
+                <span class="badge">{{hero.id}}</span> {{hero.name}}
+            </li>
+        </ul>
         <h2>{{hero.name}} details!</h2>
         <div>
             <label>id:</label>{{hero.id}}
@@ -16,7 +22,57 @@ import { Component } from '@angular/core';
             <label>name:</label>
             <input [(ngModel)]="hero.name" placeholder="name">
         </div>
-    `
+    `,
+    styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
+
 })
 export class AppComponent {
     title = 'Tour of Heroes';
@@ -24,4 +80,17 @@ export class AppComponent {
         id: 1,
         name: 'Windstorm',
     };
+    heroes = HEROS;
 }
+
+const HEROS: Hero[] = [
+    {id: 11, name: 'Mr Laravel1'},
+    {id: 12, name: 'Mr Laravel2'},
+    {id: 13, name: 'Mr Laravel3'},
+    {id: 14, name: 'Mr Laravel4'},
+    {id: 15, name: 'Mr Laravel5'},
+    {id: 16, name: 'Mr Laravel6'},
+    {id: 17, name: 'Mr Laravel7'},
+    {id: 18, name: 'Mr Laravel8'},
+];
+
