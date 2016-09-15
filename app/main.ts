e937677f0e140993d352a556c69a28350ce575e7
Xiang Liu <xiang.liu@rightcapital.com>
2016-09-15 19:54:21 +0800

    Add the hero editor

---
+++ app/main.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by liuxiang on 9/15/16.
+ */
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
